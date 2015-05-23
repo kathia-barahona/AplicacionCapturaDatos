3f04b8e88d258f83cb0bb88dc8be14818bc6cd0c
Kathia Barahona <katbara03@gmail.com>
2015-05-23 10:00:36 -0600

    Controllers for Login

---
+++ MediaDataApp/app/views/layouts/app.js
@@ -0,0 +1,20 @@
+(function() {
+    var app = angular.module('entry',[]);
+    app.controller('EntryController',function(){
+        this.user = user;
+    });
+    app.controller('RegistryController',function(){
+        this.value = 0;
+        this.active = function(value){
+            this.value = value;
+        };
+        this.isAllowed = function(){
+            if (this.value===1) {
+                return true;
+            }
+             return false;
+        };
+    });
+    var user = [{
+    }];
+})();
