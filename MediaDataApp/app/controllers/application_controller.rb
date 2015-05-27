eea7fcdb3c6097773a1feb8a91dc7a34f393a986
Xavier Munguia <xavier2696@gmail.com>
2015-05-27 15:05:02 -0600

    borrar css repetido

--- MediaDataApp/app/controllers/application_controller.rb
+++ MediaDataApp/app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
 
    before_filter :configure_permitted_parameters, if: :devise_controller?
 
-   
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
