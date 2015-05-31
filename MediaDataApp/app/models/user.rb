902f27922000856383cdd2375e53b4a50bd0cfcb
kathybarahona03 <katbara03@gmail.com>
2015-05-31 07:59:01 -0600

    picture uploading

--- MediaDataApp/app/models/user.rb
+++ MediaDataApp/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :picture, styles: { medium: "300x300>", thumb:"100x100>" }
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
