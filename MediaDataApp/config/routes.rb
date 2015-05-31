8c270cf25318ce8f749607397f14869db52492b6
kathybarahona03 <katbara03@gmail.com>
2015-05-30 18:06:47 -0600

    actualizacion del menubar

--- MediaDataApp/config/routes.rb
+++ MediaDataApp/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :registros
   devise_for :users
+  resources :users
+  get 'users/show/:id' => 'user#show', :as => 'show_user_path'
   get 'static_pages/home'
  
   # The priority is based upon order of creation: first created -> highest priority.
