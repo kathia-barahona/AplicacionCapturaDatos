ae57ce1d5e835f5755467b5b3ba270d89eb01a8a
kathybarahona03 <katbara03@gmail.com>
2015-05-31 19:39:59 -0600

    avance en profile

--- MediaDataApp/app/models/user.rb
+++ MediaDataApp/app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  has_many :registros
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+ has_attached_file :picture, styles: { medium: "300x300>", thumb:"100x100>" }
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+
+
 end
