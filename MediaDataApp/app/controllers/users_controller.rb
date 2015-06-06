c74888c97660e7e2616e3951bdd4f5a947342b14
kathybarahona03 <katbara03@gmail.com>
2015-06-06 12:27:28 -0600

    documentos

--- MediaDataApp/app/controllers/users_controller.rb
+++ MediaDataApp/app/controllers/users_controller.rb
@@ -10,4 +10,5 @@ class UsersController < ApplicationController
 	def user_params
  		 params.require(:user).permit(:picture)
 	end
+
 end
--- MediaDataApp/app/models/user.rb
+++ MediaDataApp/app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   has_many :registros
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
- has_attached_file :picture, styles: { medium: "300x300>", thumb:"100x100>" }
+ has_attached_file :picture, styles: { medium: "300x300>", thumb:"225x225>" },
+  :default_url => ":style/defaultprof.jpg"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
 
