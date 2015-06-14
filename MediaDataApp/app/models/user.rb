6c78c81a772667b4f62da93f1ca712e00b72fb28
kathybarahona03 <katbara03@gmail.com>
2015-06-14 10:22:35 -0600

    Side Menu Bar Listo

--- MediaDataApp/app/models/user.rb
+++ MediaDataApp/app/models/user.rb
@@ -1,19 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-<<<<<<< HEAD
+
  require 'carrierwave/orm/activerecord'
   has_many :registros
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :picture, PictureUploader
 end
-=======
 
-  has_many :registros
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :picture, styles: { medium: "300x300>", thumb:"100x100>" }
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
-end
->>>>>>> origin/master
+
