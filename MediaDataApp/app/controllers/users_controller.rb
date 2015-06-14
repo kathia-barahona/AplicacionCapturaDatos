331845fedfec35ee85c6c4d167d04f3ad3e09fbd
Xavier Munguia <xavier2696@gmail.com>
2015-06-14 17:52:39 -0600

    usuario admin crea otros admin

--- MediaDataApp/app/controllers/users_controller.rb
+++ MediaDataApp/app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 	before_filter :authenticate_user!
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 	def index
+		@users = User.all
     end
 	def show
 		 @user = User.find(params[:id])
@@ -13,6 +15,28 @@ class UsersController < ApplicationController
  		 params.require(:user).permit(:picture)
 	end
 
+	def edit
+		#@users = User.all
+	end
 
+	def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+	def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:admin)
+    end
 
 end
